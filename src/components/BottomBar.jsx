1608f81c29d670f36165036edb37b6b796e9e1fe
jiucchu <jiucchu@gmail.com>
2024-08-26 23:24:03 +0900

    fix : 바텀바 수정

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -13,25 +13,31 @@ const BottomBar = () => {
         <div className={`BarMenu ${location.pathname === '/' ? 'active' : ''}`}>
           <Link to='/'>
             <div className='material-symbols-outlined barImg'>deceased</div>
-            <div className='BarText'>Florist</div>
+            <div className='BarText'>꽃다발</div>
           </Link>
         </div>
         <div className={`BarMenu ${location.pathname === '/plants' ? 'active' : ''}`}>
           <Link to='/plants'>
             <div className='material-symbols-outlined barImg'>import_contacts</div>
-            <div className='BarText'>Planting</div>
+            <div className='BarText'>식물정보</div>
           </Link>
         </div>
         <div className={`BarMenu ${location.pathname === '/store' ? 'active' : ''}`}>
           <Link to='/store'>
             <div className='material-symbols-outlined barImg'>storefront</div>
-            <div className='BarText'>Store</div>
+            <div className='BarText'>스토어</div>
           </Link>
         </div>
         <div className={`BarMenu ${location.pathname === '/communities' ? 'active' : ''}`}>
           <Link to='/communities'>
             <div className='material-symbols-outlined barImg'>forum</div>
-            <div className='BarText'>Chat</div>
+            <div className='BarText'>커뮤니티</div>
+          </Link>
+        </div>
+        <div className={`BarMenu ${location.pathname === '/mypage' ? 'active' : ''}`}>
+          <Link to='/mypage'>
+            <div className='material-symbols-outlined barImg'>person</div>
+            <div className='BarText'>My</div>
           </Link>
         </div>
       </div>
