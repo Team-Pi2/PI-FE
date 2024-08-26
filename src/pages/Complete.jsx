1cd234239e227a0246b0b159fbc80ced5239bc5a
JeongSeoyun <xeonj@naver.com>
2024-08-26 19:31:21 +0900

    design: 꽃다발 완성 화면 세부 가격 애니메이션 추가

--- src/pages/Complete.jsx
+++ src/pages/Complete.jsx
@@ -38,13 +38,11 @@ const Complete = () => {
                     <span className={`material-symbols-outlined priceIcon ${isOpen ? 'open' : ''}`}>play_arrow</span>
                     <div className='completePrice'>{price}원</div>
                 </div>
-                {isOpen && (
-                        <div className='completeSubPriceContainer'>
-                            <div className='completeSubPriceTitle'>현재 꽃 시세</div>
-                            <div className='completeSubPriceBody'>튤립 5000원</div>
-                            <div className='completeSubPriceBody'>장미 3000원</div>
-                        </div>
-                    )}
+                <div className={`completeSubPriceContainer ${isOpen ? 'open' : ''}`}>
+                    <div className='completeSubPriceTitle'>현재 꽃 시세</div>
+                    <div className='completeSubPriceBody'>튤립 5000원</div>
+                    <div className='completeSubPriceBody'>장미 3000원</div>
+                </div>
             </div>
         </div>
         
